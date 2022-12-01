d93243d085f1b0755b374ddfa01c388da47a6490
dariapetrenko <118117503+dariapetrenko@users.noreply.github.com>
2022-12-01 16:44:21 +0200

    Add files via upload

---
+++ Циклічні оператори/Завдання №1.cpp
@@ -0,0 +1,48 @@
+/*™ дан≥ про ш≥сть товар≥в. ввести числовий код одного з них ≥ вивести 
+про нього так≥ в≥домост≥: ц≥ну ≥ к≥льк≥сть товару на склад≥;*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 1;
+	int b = 2;
+	int c = 3;
+	int d = 4;
+	int e = 5;
+	int f = 6;
+	double code_t;
+
+	cout << "¬вед≥ть код товару: " << endl;
+	cin >> code_t;
+
+	if (code_t == a) {
+		cout << "3$ per pack, 230 packs" << endl;
+
+	}
+	else if (code_t == b) {
+		cout << "25$ per pack, 560 packs" << endl;
+
+	}
+	else if (code_t == c) {
+		cout << "43$ per pack, 400 packs" << endl;
+
+	}
+	else if (code_t == d) {
+		cout << "10$ per pack, 1200 packs" << endl;
+
+	}
+	else if (code_t == e) {
+		cout << "23$ per pack, 256 packs" << endl;
+
+	}
+	else if (code_t == f) {
+		cout << "5$ per pack, 456 packs" << endl;
+	}
+	else {
+		cout << "none" << endl;
+	}
+	return 0;
+}
