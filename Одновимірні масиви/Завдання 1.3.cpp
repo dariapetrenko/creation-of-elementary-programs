61b85dec5b86b0533d12183fc60847bc5a344e52
dariapetrenko <118117503+dariapetrenko@users.noreply.github.com>
2022-12-06 00:39:49 +0200

    Add files via upload

---
+++ Одновимірні масиви/Завдання 1.3.cpp
@@ -0,0 +1,71 @@
+/*3. В одномірному масиві, що складається з дійсних елементів, обчислити суму від’ємних елементів 
+масиву; добуток елементів масиву, розташованих між максимальним і мінімальним елементами. 
+Упорядкувати елементи масиву за зростанням.*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cout << "n= ";
+    cin >> n;
+    if (n < 1) {
+        cout << "try again";
+    }
+    else {
+        float array[n];
+
+        for (int i = 0; i < n; i++) {
+            cin >> array[i];
+        }
+
+        float min = array[0], max = array[0], maxi = 0, mini = 0; //min ta max
+
+        for (int i = 0; i < n; i++) {
+            if (array[i] > max) {
+                max = array[i];
+                maxi = i;
+            }
+            if (array[i] < min) {
+                min = array[i];
+                mini = i;
+            }
+        }
+        float sumaVid = 0, dobutok = 1;
+
+        for (int i = 0; i < n; i++) {
+            if (array[i] < 0) sumaVid = sumaVid + array[i];
+        }
+
+        int startfrom = 0, endat = 0; //добуток
+
+        if (maxi > mini) { startfrom = mini; endat = maxi; }
+        else { startfrom = maxi; endat = mini; }
+
+        for (int i = startfrom + 1; i < endat; i++) {
+            dobutok = dobutok * array[i];
+        }
+
+        cout << "Сума від’ємних елементів масиву: " << sumaVid << endl;
+        cout << "Добуток між мін та макс: " << dobutok;
+
+        //за зростанням
+        int temp;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        cout << '\n' << "Масив: ";
+        for (int i = 0; i < n; i++) {
+            cout << array[i] << " ";
+        }
+    }
+    return 0;
+}
